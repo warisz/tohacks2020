09a511fa99fda3bdedb666269cfc0a5a1b32fc3f
warisz <wariszahoor0@gmail.com>
2020-05-03 06:16:00 -0400

    site implementation

--- flask_user/camera.py
+++ flask_user/camera.py
@@ -31,8 +31,9 @@ def detect_faces(path):
         vertices = (['({},{})'.format(vertex.x, vertex.y)
                     for vertex in face.bounding_poly.vertices])
 
-        #[face bounds, anger, joy, surprise, sorrow] 
-        faceArray = [(vertices), likelihood_name[face.anger_likelihood], likelihood_name[face.joy_likelihood],  likelihood_name[face.surprise_likelihood], likelihood_name[face.sorrow_likelihood]]
+        #[anger, joy, surprise, sorrow] 
+        faceArray = {"anger":likelihood_name[face.anger_likelihood], "joy":likelihood_name[face.joy_likelihood],  "surprise":likelihood_name[face.surprise_likelihood], "sorrow": likelihood_name[face.sorrow_likelihood]}
+        print(faceArray)
         return faceArray
 
     if response.error.message:
@@ -50,22 +51,28 @@ class VideoCamera(object):
         self.video.release()
 
     def change_frame(self):
-        global faceArray
         ret, frame = self.video.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         face = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(35, 35))
 
+        submit = ""
         for (x, y, w, h) in face:
             cv2.rectangle(frame, (x, y), (w+x, h+y), (200, 50, 30), 2)
             cropped_face = frame[y:h+y, x:w+x]
             cv2.imwrite('./faces/currentface.jpg', cropped_face)
-        
-        detect_faces("./faces/currentface.jpg")
+            
+            emotions = detect_faces("./faces/currentface.jpg")
+            
+            if emotions is not None:
+                for key, value in emotions.items():
+                    if value=="VERY_LIKELY" or value=="LIKELY" or value =="POSSIBLE":
+                        cv2.putText(frame,str(key),(x,y),cv2.FONT_HERSHEY_COMPLEX,1.5,(50,50,200),2)
+                        submit = key
 
-  
+
         ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes()
+        return [jpeg.tobytes(), submit]
 
         
 
--- flask_user/user/routes.py
+++ flask_user/user/routes.py
@@ -1,9 +1,14 @@
-from flask import render_template, Blueprint, Response
+from flask import render_template, Blueprint, Response, redirect, url_for
 from user.forms import UserForm
 from camera import VideoCamera
 import time
+import matplotlib
+import matplotlib.pyplot as mpl
+import numpy as np
 
 users = Blueprint('users', __name__)
+emotionDict = {"joy": 0, "surprise": 0, "sorrow": 0, "anger": 0}
+allEmotionDicts = {}
 
 @users.route("/", methods = ['GET', 'POST'])
 def index():
@@ -11,10 +16,40 @@ def index():
     form = UserForm()
 
     if form.validate_on_submit():
-        # TODO: Logic for the opencv stuff
+        allEmotionDicts[str(UserForm.url)] = emotionDict
+        graphing()
+        reset()
         pass
-
+        
+    
     return render_template("test.html", form = form)
+
+def graphing():
+
+    counter = 0
+    for key, value in allEmotionDicts.items():
+        #creates a new figure
+        figure = mpl.figure()
+        figure.suptitle("Emotions for " + str(key))
+
+        figure, axes = mpl.subplots(1,1) #makes a 1x1 figure
+
+        #setting axis labels
+        axes.set_xlabel('Emotions')
+        axes.set_ylabel('Seconds')
+        axes.set_ylim(0,100)
+
+        #plotting a point - 'bo' means blue plot
+        for key2, value2 in value.items():
+            axes.plot(key2, value2, 'bo')
+
+
+        #CREATES FILE ---> CHECK gradesGraph.png
+        figure.savefig("graph" + str(counter) + ".png")
+        counter+=1
+
+def reset():
+    emotionDict = {"joy": 0, "surprise": 0, "sorrow": 0, "anger": 0}
 
 def gen(camera):
     start = time.time()
@@ -22,7 +57,13 @@ def gen(camera):
         delay = 3
         if (time.time() - start) >= delay:
 
-            frame = camera.change_frame()
+            frame = (camera.change_frame())[0]
+            emotion = (camera.change_frame())[1]
+
+            if emotion != "":
+                emotionDict[emotion] += 1
+
+            print(emotionDict)
             yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
                
@@ -31,3 +72,4 @@ def gen(camera):
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
